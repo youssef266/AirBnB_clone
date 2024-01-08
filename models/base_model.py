bd4299ca27c4f2ac3797be4185d41e0880749991
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-01-08 22:06:45 +0200

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,41 @@
 #!/usr/bin/python3
+import uuid
+from datetime import datetime
 """
 till we finish the class
 """
+
 
 class BaseModel:
     """define the attributes and methods"""
     def __init__(self):
         """
+        this is the attributes for the basemodel
         """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """updates the public instance attribute,
+        updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Convert the object to a dictionary representation.
+        Returns:
+            dict: A dictionary containing the object's attributes,
+            class name, and ISO-formatted date strings.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+
+        return obj_dict
+
+    def __str__(self):
+        """Custom string representation of the object"""
+        return ("[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__))
