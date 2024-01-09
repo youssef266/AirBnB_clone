c7437940f17a3b03e86a9ef226d9a0fe92a6ff1f
ayaelas <assalharras@gmail.com>
2024-01-09 15:04:52 +0100

    storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Define a class"""
+
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.user import User
+from models.base_model import BaseModel
+import json
