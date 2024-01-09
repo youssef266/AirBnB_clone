9406211385a526af6e900896e3bc5bcba080b0a8
ayaelas <assalharras@gmail.com>
2024-01-09 13:09:12 +0100

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+
+dt_format = "%Y-%m-%d %H:%M:%S"
 """
 till we finish the class
 """
@@ -8,13 +10,23 @@ till we finish the class
 
 class BaseModel:
     """define the attributes and methods"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
-        this is the attributes for the basemodel
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs: 
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ['updated_at', 'created_at']:
+                        setattr(self, key, datetime.strptime(value, dt_format))
+                    else:
+                        setattr(self, key, value)
+        else:
+            """
+            these are the attributes for the basemodel
+            """
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """updates the public instance attribute,
