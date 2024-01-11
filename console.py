b9433314683df9f8d28b69d6e87ec04f5786da42
ayaelas <assalharras@gmail.com>
2024-01-11 14:38:32 +0100

    als

--- console.py
+++ console.py
@@ -9,7 +9,7 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """it's the Quit command to exit the program"""
+        """It is Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
