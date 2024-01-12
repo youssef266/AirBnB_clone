ecc266957da3326ca9e9363ffc1b3dc213a05806
ayaelas <assalharras@gmail.com>
2024-01-12 13:41:48 +0100

    task 7 create

--- console.py
+++ console.py
@@ -4,8 +4,26 @@ and it commands
 """
 import cmd
 
+from models.base_model import BaseModel
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
+    classes = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review
+    }
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
@@ -20,6 +38,19 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """an command for Do nothing on an empty line"""
         pass
+    
+    def do_create(self, name):
+        if name:
+            if name in calsses.keys():
+                new = classes[name]()
+                new.save()
+                print(new.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+            
+        
 
 
 if __name__ == '__main__':
