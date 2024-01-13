b61d3efb4534aa0955e2d445ea335b067cc0ecbf
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-01-13 22:35:33 +0200

    task 7 update

--- console.py
+++ console.py
@@ -13,8 +13,6 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
-
-
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
     
@@ -29,11 +27,11 @@ class HBNBCommand(cmd.Cmd):
         }
     
 
-    def do_quit(self, name):
+    def do_quit(self, arg):
         """It is Quit command to exit the program"""
         return True
 
-    def do_EOF(self, name):
+    def do_EOF(self, arg):
         """it's the Exit the program when EOF is encountered"""
         print()  # Print a new line before exiting
         return True
@@ -115,6 +113,56 @@ class HBNBCommand(cmd.Cmd):
                 elif len(argl) == 0:
                     objl.append(obj.__str__())
             print(objl)
+    
+    def do_update(self, arg):
+        """Usage: update <class> <id> <attribute_name> <attribute_value> or
+       <class>.update(<id>, <attribute_name>, <attribute_value>) or
+       <class>.update(<id>, <dictionary>)
+        Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary."""
+        argl = arg.split()
+        objdict = storage.all()
+
+        if len(argl) == 0:
+            print("** class name missing **")
+            return False
+        if argl[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return False
+        if len(argl) == 1:
+            print("** instance id missing **")
+            return False
+        if "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
+            print("** no instance found **")
+            return False
+        if len(argl) == 2:
+            print("** attribute name missing **")
+            return False
+        if len(argl) == 3:
+            try:
+                type(eval(argl[2])) != dict
+            except NameError:
+                print("** value missing **")
+                return False
+
+        if len(argl) == 4:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            if argl[2] in obj.__class__.__dict__.keys():
+                valtype = type(obj.__class__.__dict__[argl[2]])
+                obj.__dict__[argl[2]] = valtype(argl[3])
+            else:
+                obj.__dict__[argl[2]] = argl[3]
+        elif type(eval(argl[2])) == dict:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            for k, v in eval(argl[2]).items():
+                if (k in obj.__class__.__dict__.keys() and
+                        type(obj.__class__.__dict__[k]) in {str, int, float}):
+                    valtype = type(obj.__class__.__dict__[k])
+                    obj.__dict__[k] = valtype(v)
+                else:
+                    obj.__dict__[k] = v
+        storage.save()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
