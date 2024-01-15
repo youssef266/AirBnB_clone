df10db394dd1f0c1b9ea6bb9cafe008fd634dfe3
ayaelas <assalharras@gmail.com>
2024-01-15 11:17:51 +0100

    test user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import unittest
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+    """
+    setUp method is a special method in unittest.TestCase that is called before each test method is executed
+    """
+    def setUp(self):
+        self.user = User()
+        
+    """
+        creating an instance of BaseModel:
+    """
+    def test_attributes(self):
+        self.assertTrue(hasattr(self.user, 'email'))
+        self.assertTrue(hasattr(self.user, 'password'))
+        self.assertTrue(hasattr(self.user, 'first_name'))
+        self.assertTrue(hasattr(self.user, 'last_name'))
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
+        
+    def test_inheritance(self):
+        from models.base_model import BaseModel
+        self.assertIsInstance(self.user, BaseModel)
+        
+    if __name__ == '__main__':
+        unittest.main()
